0a2e385f7b4e477cd20e306b31d0600e91896826
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-08-26 16:24:57 -0500

    chagnes

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -3,19 +3,7 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on("turbolinks:load", function(){
-      $(".button-collapse").sideNav();
-      $('.materialboxed').materialbox();
 
-      $("#schedule").on("click", function(){
-        if($("#expanded").hasClass("hide")){
-          $("#expanded").removeClass("hide");
-        }else{
-          $("#expanded").addClass("hide");
-        };
-      });
 
-      $('.datepicker').pickadate({
-         selectMonths: true, // Creates a dropdown to control month
-         selectYears: 15 // Creates a dropdown of 15 years to control year
-       });
+
 })

3bf2d484aaad283417df16a67bc468525465bf9e
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-08-30 21:02:19 -0500

    update production mailing to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,13 +60,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'outloudrock.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'outloudrock.com',
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

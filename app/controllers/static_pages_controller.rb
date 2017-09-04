2ac5209aecd013297562e2d009e9d5491236f90e
Kyle Laughlin <kylejlaughlin@icloud.com>
2017-09-04 14:19:13 -0500

    utilize httparty rather than net

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,22 +9,23 @@ class StaticPagesController < ApplicationController
 
   def photos
     #Create the request url to obtain the access token
-    uri = URI.parse("https://graph.facebook.com/v2.7/oauth/access_token?client_id=" + ENV["APP_ID"] +
+    url = "https://graph.facebook.com/v2.7/oauth/access_token?client_id=" +     ENV["APP_ID"] +
                     "&client_secret=" + ENV["APP_SECRET"] +
-                    "&grant_type=client_credentials")
+                    "&grant_type=client_credentials"
     #Send request
-    response = Net::HTTP.get_response(uri)
-    response = JSON.parse(response.body)
+    raw_response = HTTParty.get(url)
+    response = JSON.parse(raw_response.body)
+
     #isolate access_token from JSON Response
     access_token = response["access_token"]
 
     #Create the request url to obtain image sources
     page_url = "https://graph.facebook.com/v2.7/118492114894836?fields=albums{name,photos.limit(1000){images,created_time},created_time}&access_token=" +
                 access_token
-    uri = URI.parse(page_url)
+
     #Send request
-    response = Net::HTTP.get_response(uri)
-    response = JSON.parse(response.body)
+    raw_response = HTTParty.get(page_url)
+    response = JSON.parse(raw_response.body)
 
     response = response["albums"]["data"].find { |a| a["name"] == "Shows: 2016 - 2017" }
     response = response["photos"]
@@ -33,11 +34,10 @@ class StaticPagesController < ApplicationController
       response["data"].each do |photo|
         @image_sources << photo["images"][0]["source"]
       end
-      next_page = response["paging"]["next"]
-      break if next_page == nil
-      uri = URI.parse(next_page)
-      response = Net::HTTP.get_response(uri)
-      response = JSON.parse(response.body)
+      next_page_url = response["paging"]["next"]
+      break if next_page_url == nil
+      raw_response = HTTParty.get(next_page_url)
+      response = JSON.parse(raw_response.body)
     end
     # #Create empty array to gather image source urls from JSON response
     # @image_sources = []
